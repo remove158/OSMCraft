87cf204ddb2090f04ac31af42ff89373a6344497
Piyaphat Pinyo <remove158@gmail.com>
2021-07-12 21:51:08 +0700

    refactor: delete un using lib

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Canvas } from "react-three-fiber";
 import { Physics } from "use-cannon";
 import { Sky } from "drei";
